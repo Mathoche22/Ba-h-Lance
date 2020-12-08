db5d397369bc8e92967cfd52280a0b4da6e50cee
Mathilde Cellier <mathilde.cellier1@free.fr>
2020-12-08 22:13:05 +0100

    Setting scripts and UI

---
+++ Assets/Player/Script/Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Shooting : MonoBehaviour
+{
+    public float healing= 10f;
+    public float range =100f;
+
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+        }
+    }
+}
